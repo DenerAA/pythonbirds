3b387217d29f867f13001e9ff3ab09f54eed1b52
denerjmj <dener@jmjsistemas.com.br>
2020-03-31 18:25:07 -0400

    criados atributos de instâncias: nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,11 @@
 class Pessoa:
+    def __init__(self, nome=None):
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Dener')
     print(p.cumprimentar())
+    print(p.nome)
